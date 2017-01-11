2449bfb2c1e9c1984bd98aa6cccd3c78b3df5fb2
Wayamon <m.wayama@kkumail.com>
2017-01-11 14:16:58 +0700

    Start

---
+++ app/src/main/java/kku/mhealth/hackathon/mpaindiaryv2/Profile.java
@@ -0,0 +1,21 @@
+package kku.mhealth.hackathon.mpaindiaryv2;
+
+/**
+ * Created by TheButterfly on 11-Jan-17.
+ */
+
+public class Profile {
+
+    public String name;
+    public String sex;
+    public String birthDate;
+
+    public Profile(){}
+
+    public Profile(String name,String sex,String birthDate){
+        this.name = name;
+        this.sex = sex;
+        this.birthDate = birthDate;
+    }
+
+}
